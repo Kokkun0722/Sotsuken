aa49d01d7d795bfa579332751599e8a1b38d33f4
kokkun <r19087@osaka-pct.ac.jp>
2023-02-27 08:44:45 +0900

    画像送受信をやってる途中

--- line_app_test_GAS/image_Uploder.py
+++ line_app_test_GAS/image_Uploder.py
@@ -1,32 +1,18 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import io
 import cv2
-from datetime import datetime
 
-# Google Sheets API情報
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/service_account_key.json', scope)
-client = gspread.authorize(creds)
+url = "https://script.google.com/macros/s/AKfycbxp8nnMsfuacXjlF8HE4RdLA4bxOZOA5Javi-lapjBAIFYtgKHIBkW-NZOHlUcpef-GqQ/exec"
 
-# スプレッドシートの情報
-sheet = client.open('Spreadsheet Name').sheet1
-
-# カメラから写真を取得する
+# カメラの設定
 cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-# 日時を取得する
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-
-# スプレッドシートにデータを追加する
-row = [dt_string, 'file_name.jpg']
-sheet.append_row(row)
-
-# ファイルを保存する
-cv2.imwrite('file_name.jpg', frame)
-
-# カメラを解放する
-cap.release()
-cv2.destroyAllWindows()
+# 画像のキャプチャと送信
+with io.BytesIO() as stream:
+    ret, frame = cap.read()
+    cv2.imwrite("image.jpg", frame)
+    files = {"file": open("image.jpg", "rb")}
+    r = requests.post(url, files=files)
+print(r)
